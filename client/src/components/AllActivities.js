0c50d72c0461447c2a10f9ceea8af20dfab1a991
tinaCanCode <mc.scherzberg@googlemail.com>
2021-01-23 16:26:13 +0100

    added a view of all activities

--- client/src/components/AllActivities.js
+++ client/src/components/AllActivities.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
+import axios from 'axios';
+import Activity from './Activity.js'
 
 class AllActivities extends Component {
 
+  state = {
+    activitiesFromDb: []
+  }
+
+  componentDidMount() {
+    axios.get("/api/activities")
+    .then(response => {
+      console.log("Response from backend: ", response.data)
+      this.setState({activitiesFromDb: response.data})
+    })
+}
+
   render() {
+    console.log(this.state.activitiesFromDb)
     return (
       <div>
-        All Activities will go here
+        <h3>All Activities</h3>
+        {this.state.activitiesFromDb.map(activity => <Activity activity={activity} />)}
       </div>
     )
   }
