ef3cb513fb24efde2a2143a8fe58490e9c2d1586
Laurence Rega <regalaurence@gmail.com>
2021-01-23 15:08:36 +0100

    16 seed activities

--- models/user-model.js
+++ models/user-model.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+
+// eslint-disable-next-line prefer-destructuring
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
   password: String,
   preferences: [String],
-  bookmarkedActivities:[{type: Schema.Types.ObjectId, ref: 'Activity'}],
-  activityCounter: Number
+  bookmarkedActivities: [{ type: Schema.Types.ObjectId, ref: 'Activity' }],
+  activityCounter: Number,
 });
 
 const User = mongoose.model('User', userSchema);
--- routes/activity-routes.js
+++ routes/activity-routes.js
@@ -1,54 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const router = express.Router();
 
 const Activity = require('../models/activity-model');
-const User = require('../models/user-model'); // <== !!!
-
-
+// const User = require('../models/user-model'); // <== !!!
 // POST route => creating a new activity
-router.post('/activities', (req, res, next) => {
-
+router.post('/activities', (req, res) => {
   Activity.create({
     name: req.body.name,
+    descrption: req.body.descrption,
     minDuration: req.body.minDuration,
-    creator: req.body.creator, //decide ? return User.findByIdAndUpdate(user, { $push: { bookmarkedActivities: dbActivity._id } });
+    creator: req.body.creator,
+    // ? return User.findByIdAndUpdate(user, { $push: { bookmarkedActivities: dbActivity._id } });
     categories: req.body.categories,
     timeWindowStart: req.body.timeWindowStart,
     timeWindowEnd: req.body.timeWindowEnd,
     hasCost: false,
     seasonStart: req.body.seasonStart,
-    seasonEnd: req.body.seasonEnd
+    seasonEnd: req.body.seasonEnd,
   })
-    .then(response => {
+    .then((response) => {
       res.json(response);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
-    })
+    });
 });
-
-
-
-
-
-
 // GET route => to get all the activities
-router.get('/activities', (req, res, next) => {
-  Activity.find().populate('user') //decide?
-    .then(allTheActivities => {
+router.get('/activities', (req, res) => {
+  Activity.find().populate('user') // decide?
+    .then((allTheActivities) => {
       res.json(allTheActivities);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
-    })
+    });
 });
 
-
-
 // GET route => to get a specific activity/detailed view
-router.get('/activities/:id', (req, res, next) => {
-
+router.get('/activities/:id', (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     res.status(400).json({ message: 'Specified id is not valid' });
     return;
@@ -57,36 +48,33 @@ router.get('/activities/:id', (req, res, next) => {
   // activities have an user' ids and
   // we can use the .populate() method to include whole users objects _______ WE DONT NEED THAT OR?
   Activity.findById(req.params.id).populate('user')
-    .then(response => {
+    .then((response) => {
       res.status(200).json(response);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
-    })
-})
+    });
+});
 
 // POST route => customize a specific ACTIVITY _________WE HAVE TO COPY IT FIRST ????
-router.put('/activities/:id', (req, res, next) => {
-
+router.put('/activities/:id', (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     res.status(400).json({ message: 'Specified id is not valid' });
     return;
   }
 
-
-  // _________WE HAVE TO COPY IT FIRST  ?????????????? WILL WE ALSO UPDATE IT WITHOUT POSTING NEW ONE??
+  // WE HAVE TO COPY IT FIRST  ?????????????? WILL WE ALSO UPDATE IT WITHOUT POSTING NEW ONE??
   Activity.findByIdAndUpdate(req.params.id, req.body)
     .then(() => {
       res.json({ message: `Activity with ${req.params.id} is updated successfully.` });
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
-    })
-})
+    });
+});
 
 // DELETE route => to delete a specific activity
-router.delete('/activities/:id', (req, res, next) => {
-
+router.delete('/activities/:id', (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     res.status(400).json({ message: 'Specified id is not valid' });
     return;
@@ -96,12 +84,9 @@ router.delete('/activities/:id', (req, res, next) => {
     .then(() => {
       res.json({ message: `Project with ${req.params.id} is removed successfully.` });
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
-    })
-})
+    });
+});
 
 module.exports = router;
-
-
-
