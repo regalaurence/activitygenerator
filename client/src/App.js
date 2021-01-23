4515ee2e96c8d00275849497800e08bd25f41448
tinaCanCode <mc.scherzberg@googlemail.com>
2021-01-23 15:28:15 +0100

    basic app structure and component placeholders added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import './App.css';
+import { Link, Route } from 'react-router-dom';
 
 //Components
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import AddActivity from './components/AddActivity';
+import CreateTodoList from './components/CreateTodoList';
+import AllActivities from './components/AllActivities';
+import MyActivities from './components/MyActivities';
 
 class App extends Component {
 
@@ -47,7 +51,16 @@ class App extends Component {
         <hr></hr>
         <h1>Make Me Do</h1>
         <h2>The things I said I'd do tomorrow</h2>
-        <AddActivity />
+        <div>
+        <Link to="/make-me-do">Make me DO something</Link><br></br>
+        <Link to="/activities">Browse activities</Link><br></br>
+        <Link to="/add-activity">Create an Activity</Link><br></br>
+        <Link to="/my-activities">My activities</Link><br></br>
+        </div>
+        <Route path="/make-me-do" component={CreateTodoList}></Route>
+        <Route path="/activities" component={AllActivities}></Route>
+        <Route path="/add-activity" component={AddActivity}></Route>
+        <Route path="/my-activities" component={MyActivities}></Route>
       </div>
     );
   }
