e664a06e98e49f7e58151924f17d73d287960a3f
Anita Mikas <mikasanita@gmail.com>
2021-01-29 13:55:55 +0100

    searchbox

--- client/src/components/AllActivities.js
+++ client/src/components/AllActivities.js
@@ -10,16 +10,49 @@ class AllActivities extends Component {
 
   componentDidMount() {
     axios.get("/api/activities")
-    .then(response => {
-      console.log("Response from backend: ", response.data)
-      this.setState({activitiesFromDb: response.data})
-    })
-}
+      .then(response => {
+        console.log("Response from backend: ", response.data)
+        this.setState({ activitiesFromDb: response.data })
+      })
+  }
+
+  handleChange=(event) =>{
+    let target = event.target;
+    let value = target.value;
+    this.setState({ 
+        search: value 
+    });
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    console.log('state here', this.state);
+};
+
+
+
 
   render() {
+
+    let allActivities = [...this.state.activitiesFromDb].filter(activity => {
+      return activity.name.toLowerCase().includes(this.state.search);
+    });
+
+
+
     console.log(this.state.activitiesFromDb)
     return (
-      <div>
+
+<div className="all-activities">
+          <form>
+            <input
+              type="text"
+              placeholder="Find activity here..."
+              name="search"
+              value={this.state.search}
+              onChange={this.handleChange}
+            />
+          </form>
+
+
+      
         <h3>All Activities</h3>
         {this.state.activitiesFromDb.map(activity => <Activity activity={activity} />)}
       </div>
