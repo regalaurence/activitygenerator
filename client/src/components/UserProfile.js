12632b2d1936db7e6e4a16e67142d6bfb8aec401
Anita Mikas <mikasanita@gmail.com>
2021-02-11 21:38:47 +0100

    changes working done

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class UserProfile extends Component {
-  newChosenPreferences = [];
+  //newChosenPreferences = [];
   state = {
     username: "",
     // password: "",
     preferences: [],
-  
   }
+
+
 
   componentDidMount = () => {
     let user = this.props.user
@@ -16,17 +17,17 @@ class UserProfile extends Component {
     this.setState({
       username: user.username,
       preferences: user.preferences,
-     
+
     })
   }
 
-  updatingUserPreferences = () => {
-
-    const preferences = this.newChosenPreferences;
-    axios.put(`/api/user/${this.props.user._id}`, { preferences })
+  updatingUserPreferences = (newPreferences) => {
+    //const newPreferences = this.newChosenPreferences;
+    // const newPreferences = [...this.state.preferences, this.newChosenPreferences];
+    axios.put(`/api/user/${this.props.user._id}`, { preferences: newPreferences })
       .then(() => {
         this.setState({
-          preferences: this.newChosenPreferences
+          preferences: newPreferences
         });
         let user = this.props.user
         user.preferences = this.newChosenPreferences
@@ -38,31 +39,33 @@ class UserProfile extends Component {
 
 
   pushToCategories = (event) => {
+    let newChosenPreferences = [...this.state.preferences]
+    let chosenActivity = event.currentTarget.value
+    if (!newChosenPreferences.includes(chosenActivity)) {
+      newChosenPreferences.push(chosenActivity)
+      // console.log(this.newChosenPreferences)
+    }
+    else {
+     let filteredPreferences = newChosenPreferences.filter(activity => activity !== chosenActivity)
+     newChosenPreferences = filteredPreferences;
+    }
 
-    let button = event.currentTarget
-
-    if(!this.newChosenPreferences.includes(button.value))
-   {this.newChosenPreferences.push(button.value)
-    console.log(this.newChosenPreferences)}
-
-    if (this.newChosenPreferences.length >= 1) {
-      this.updatingUserPreferences()
+    if (newChosenPreferences.length >= 1) {
+      this.updatingUserPreferences(newChosenPreferences)
     }
   }
 
 
 
   creatingButtonsFunction = () => {
-    let possiblePreferences = ["Sports", "Relaxing", "Indoors", "Housework", "Socializing", "Adventures", "Indoors", "Online",];
+    let possiblePreferences = ["Sports", "Relaxing", "Indoors", "Housework", "Socializing", "Adventures", "Outdoors", "Online",];
     return possiblePreferences.map(pref => {
-        return (
-          <div className="column has-text-centered is-one-quarter-mobile is-one-quarter-tablet is-one-quarter-desktop is-one-quarter-widescreen is-one-quarter-fullhd">
-            <button className="button is-primary" key={pref} type="submit" onClick={this.pushToCategories} value={pref}>{pref}</button>
-          </div>)
-
-      }
-      )
-
+      return (
+        <div className="column has-text-centered is-one-quarter-mobile is-one-quarter-tablet is-one-quarter-desktop is-one-quarter-widescreen is-one-quarter-fullhd">
+          <button className="button is-primary" key={pref} type="submit" onClick={this.pushToCategories} value={pref}>{pref}</button>
+        </div>)
+    }
+    )
   }
 
   render() {
@@ -70,26 +73,26 @@ class UserProfile extends Component {
       <section className="hero is-fullheight">
         <div className="hero-body">
           <div className="container">
-          <div className="columns is-vcentered is-centered">
-          <div className="is-vcentered is-centered">
-          <div className="content has-text-centered">
-          <h2 className="is-small">Username: {this.state.username}</h2></div>
-          <div className="content has-text-centered">
-          <h5 className="is-small">Your preferences: </h5> {this.state.preferences.map(pref => {
-          return <p>{pref}</p>  
-          })}
-          
-          </div>
-          <div className="columns is-multiline is-vcentered is-centered has-text-centered">
-        {this.creatingButtonsFunction()}
-        </div>
-        {/* <div className="column has-text-centered">
+            <div className="columns is-vcentered is-centered">
+              <div className="is-vcentered is-centered">
+                <div className="content has-text-centered">
+                  <h2 className="is-small">Username: {this.state.username}</h2></div>
+                <div className="content has-text-centered">
+                  <h5 className="is-small">Your preferences: </h5> {this.state.preferences.map(pref => {
+                    return <p>{pref}</p>
+                  })}
+
+                </div>
+                <div className="columns is-multiline is-vcentered is-centered has-text-centered">
+                  {this.creatingButtonsFunction()}
+                </div>
+                {/* <div className="column has-text-centered">
         <button className="button is-warning" key="submitNewPreferences" type="submit" onClick={this.updatingUserPreferences}>Save changes</button>
       </div> */}
-      </div>
-      </div>
-      </div>
-      </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     )
   }
