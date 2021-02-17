ce057a1be4cc77b2d07e50d91f0885095f8a9f22
Anita Mikas <mikasanita@gmail.com>
2021-02-17 21:44:52 +0100

    navbar working but not perfectly

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,10 +4,34 @@ import { withRouter, Redirect } from 'react-router-dom'
 
 
 class Navbar extends Component {
+  
+    state = {
+      active: "navbar-menu"
+    }
+  
 
-handleLogOut = () => {
-  this.props.logoutUser()
-}
+  toggleClass = () => {
+    console.log("happens")
+    console.log(this.state.active)
+    if (this.state.active == "navbar-menu") {
+      return this.setState({
+        active: "navbar-menu is-active"
+      })
+    }
+    else {
+      return this.setState({
+        active: "navbar-menu"
+      })
+    }
+  }
+
+
+  handleLogOut = () => {
+    this.props.logoutUser()
+  }
+
+
+
 
   render() {
 
@@ -18,68 +42,77 @@ handleLogOut = () => {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <a className="navbar-item">  
-          <Link to="/home" className="has-text-grey-dark">
-          <img src="images/MakeMeDo_textOnly_black.png" width="112" height="28" />
-          </Link>
+          <a className="navbar-item">
+            <Link to="/home" className="has-text-grey-dark">
+              <img src="images/MakeMeDo_textOnly_black.png" width="112" height="28" />
+            </Link>
           </a>
 
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          
-  
-            <span aria-expanded="true"></span>
-            <span aria-expanded="true"></span>
-            <span aria-expanded="true"></span>
+          <a role="button" onClick={this.toggleClass} className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+
+
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </a>
+
         </div>
-        
-        <div id="navbarBasicExample" className="navbar-menu">
+
+
+        {/* <input type="checkbox" id="nav-toggle-state" /> */}
+
+
+        <div id="navbarBasicExample" className={this.state.active}>
           <div className="navbar-start">
             <a className="navbar-item">
-              <Link to="/home" className="has-text-grey-dark">Home</Link>
+              <Link to="/home" className="has-text-grey-dark" onClick={this.toggleClass}>Home</Link>
             </a>
 
             <div className="navbar-start">
-            <a className="navbar-item">
-              <Link to="/make-me-do" className="has-text-grey-dark">Do Something</Link>
-            </a>
+              <a className="navbar-item">
+                <Link to="/make-me-do" className="has-text-grey-dark" onClick={this.toggleClass}>Do Something   </Link>
+              </a>
             </div>
 
-            <div className="navbar-item has-dropdown is-hoverable">
+      
+
+<nav class="navbar" role="navigation" aria-label="dropdown navigation">
+            <div className="navbar-item has-dropdown is-hoverable" >
               <a className="navbar-link">
                 Activities
             </a>
-
-              <div className="navbar-dropdown">
-              <a className="navbar-item">
-              <Link to="/activities" className="has-text-grey-dark">Browse Activities</Link>
-            </a>
-            <a className="navbar-item">
-              <Link to="/my-activities" className="has-text-grey-dark">My Activities</Link>
-            </a>
-            <a className="navbar-item">
-              <Link to="/add-activity" className="has-text-grey-dark">Create Activities</Link>
-            </a>
+              <div className="navbar-dropdown is-right">
+                <a className="navbar-item">
+                  <Link to="/activities" className="has-text-grey-dark" onClick={this.toggleClass}>Browse Activities</Link>
+                </a>
+                <a className="navbar-item">
+                  <Link to="/my-activities" className="has-text-grey-dark" onClick={this.toggleClass}>My Activities</Link>
+                </a>
+                <a className="navbar-item">
+                  <Link to="/add-activity" className="has-text-grey-dark" onClick={this.toggleClass}>Create Activities</Link>
+                </a>
               </div>
             </div>
+</nav> 
+
           </div>
 
           <div className="navbar-end">
-          <a className="navbar-item">
-          {/* ANITA CHENGE HERE FOR USER PROFILE */}
-              {this.props.currentUser ? <div to="/make-me-do" className="has-text-grey-dark"><Link to="/user-profile" className="button-is-light">Your profile</Link></div> : null}
+            <a className="navbar-item">
+              {/* ANITA CHENGE HERE FOR USER PROFILE */}
+              {this.props.currentUser ? <div to="/make-me-do" className="has-text-grey-dark"><Link to="/user-profile" className="button-is-light" onClick={this.toggleClass}>Your profile</Link></div> : null}
             </a>
             <div className="navbar-item">
               <div className="buttons">
-              {!this.props.currentUser ? 
-                <a className="button is-primary">
-                  <Link to="/signup" className="has-text-grey-dark">Sign up</Link>
-                </a> : null}
-                {/* PROBLEMS HERE PROBLEMS HERE PROBLEMS HERE PROBLEMS HERE */}
+                {!this.props.currentUser ?
+                  <a className="button is-primary">
+                    <Link to="/signup" className="has-text-grey-dark" onClick={this.toggleClass}>Sign up</Link>
+                  </a> : null}
+
                 {this.props.currentUser ? <a className="button is-light" onClick={this.handleLogOut}>
                   Log out</a> : <a className="button is-light">
-                  <Link to="/login" className="button-is-light">Log in</Link>
-              </a>}
+                    <Link to="/login" className="button-is-light">Log in</Link>
+                  </a>}
               </div>
             </div>
           </div>
