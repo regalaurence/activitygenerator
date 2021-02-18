b20cd35607fec6078013a1fb0f92f27076f65fbc
Laurence Rega <regalaurence@gmail.com>
2021-02-18 14:39:04 +0100

    changed homepage

--- client/src/components/homepage/About.js
+++ client/src/components/homepage/About.js
@@ -8,23 +8,22 @@ class About extends React.Component {
   render() {
     return (
     
-          <div className="column " style={{ maxWidth: "512px" }}>
+          <div className="column has-text-centered" style={{ maxWidth: "512px" }}>
             <div className="title is-3">
               <h1>Welcome to <strong>Make Me Do!</strong></h1>
             </div>
-            <div>
-              <p>Ever had some free time on your hands but no idea what to do with it, hence did nothing at all and ended up regretting it?</p>
-              <p>Those days are over! <br />
-                <strong>Make Me Do!</strong> comes to the rescue, by generating tailor-made to-do-lists according to how much free time you have ahead.</p>
-              <p>Yup, tailor made. JUST-FOR-YOU. <br></br>
+            <div >
+              <p>Ever had some free time on your hands but no idea what to do with it, hence did nothing at all and ended up regretting it? Those days are over! <br /><br />
+                <strong>Make Me Do!</strong> comes to the rescue, by generating tailor-made to-do-lists according to how much free time you have ahead.
+              Yup, tailor made. JUST-FOR-YOU. <br/><br/>
               
                 Browsing our awesome activities database, matching it with your preferences,
                 letting you add, edit and bookmark activities,
                 crosschecking how much of a priority it is for you to do this or that
-                and much more.<br></br>
+                and much more.<br/><br/>
               
-              We promise you, "I dont' know what to do" won't be part of your vocabulary anymore.</p>
-              <p>Because, honey... <strong>Make Me Do !</strong> will make you do.</p>
+              We promise you, "I dont' know what to do" won't be part of your vocabulary anymore.
+              <br /><br />Because, honey... <strong>Make Me Do !</strong> will make you do.</p>
             </div>
             <div>
               <Link to="/signup"><button className="button is-primary mr-3 mt-4">Sign me up!</button></Link>
--- client/src/components/homepage/Welcome.js
+++ client/src/components/homepage/Welcome.js
@@ -6,27 +6,22 @@ class Welcome extends React.Component {
   render() {
     return (
       <section className="hero is-fullheight">
-      <div className="hero-body">
-        <div className="container">
-          <div className="columns is-vcentered is-centered center">
-
-
-          
-            
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-vcentered is-centered center">
               <figure className="image mb-4">
                 <img style={{ maxWidth: "312px" }} src="images/BlueCloudDarkSignup.png" />
               </figure>
-</div>
+            </div>
             <div className="columns is-vcentered is-centered center">
-            
-      <About/>
-      </div>
+              <About />
+            </div>
           </div>
-      </div>
-      
+        </div>
+
       </section>
     )
-}
+  }
 }
 
 export default Welcome
