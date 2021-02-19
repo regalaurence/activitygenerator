4bfec8f0dff1a5b73755dc4081d83902097a49c6
Laurence Rega <regalaurence@gmail.com>
2021-02-19 14:50:33 +0100

    categories are green when selected in preferences and create activity form

--- client/src/components/createactivities/CategoriesCheckboxes.js
+++ client/src/components/createactivities/CategoriesCheckboxes.js
@@ -1,34 +1,36 @@
 import React from 'react';
 
-const CategoriesCheckboxes = (props) => {
+class CategoriesCheckboxes extends React.Component {
 
-  // props.value = ['Sports']
-  let availableCategories = ['Indoors', 'Outdoors', 'Sports', 'Adventures', 'Housework', 'Socializing', 'Relaxing', 'Online']
+  render() {
+    // props.value = ['Sports']
+    let availableCategories = ['Indoors', 'Outdoors', 'Sports', 'Adventures', 'Housework', 'Socializing', 'Relaxing', 'Online']
 
-  // let isChecked = (name) => {
-  //   return props.value.includes(name)
-  // }
+    let isChecked = (name) => {
+      if (this.props.value.includes(name)) {
+        return  "btn-selected"
+      }
+      else return "btn-not-selected"
+    }
 
- 
-
-
-
-  return (
-    <div className="columns is-multiline is-mobile">
-      {/* <label>Choose categories:</label> */}
-      {availableCategories.map(c => {
-        return (
-          <div className="column is-one-third-mobile is-one-quarter-tablet is-one-quarter-desktop is-one-quarter-widescreen is-one-quarter-fullhd has-text-centered">
-          <button id="categories" className="has-text-centered button-checkboxes" type="checkbox">
-          <figure className="image column is-vcentered is-centered">
-              <img id="categories" alt="category" className="image" name={c} style={{ maxWidth: "180px" }} src={"images/" + c + ".png"} onClick={props.onChange}/>
-            </figure>
-            </button><br />{c}
-          </div>
-        )
-      })}
-    </div>
-  )
+    return (
+      <div className="columns is-multiline is-mobile">
+        {/* <label>Choose categories:</label> */}
+        {availableCategories.map(c => {
+          console.log("CHECK THIS NAME:" + c)
+          return (
+             <div className="column is-one-third-mobile is-one-quarter-tablet is-one-quarter-desktop is-one-quarter-widescreen is-one-quarter-fullhd has-text-centered">
+              <button id="categories" value={c} className={isChecked(c)} type="checkbox">
+                <figure className="image column is-vcentered is-centered">
+                  <img id="categories" alt="category" className="image" name={c} style={{ maxWidth: "180px" }} src={"images/" + c + ".png"} onClick={this.props.onChange} />
+                </figure>
+              </button><br />{c}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default CategoriesCheckboxes;
