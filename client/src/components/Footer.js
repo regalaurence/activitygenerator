79de8e8fbd7db8a33c19f48b955f5093087f0c63
Laurence Rega <regalaurence@gmail.com>
2021-02-20 10:32:29 +0100

    footer text2

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,8 +7,8 @@ class Footer extends Component {
 <footer className="footer mt-5" style={{ maxHeight: "122px" }}>
   <div className="content has-text-centered is-small">
     <p>
-      <strong>Make me do!</strong> by Anita, Laurence and Tina. <br></br>The source code is licensed
-      <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
+      <strong>Make me do!</strong> by Anita, Laurence and Tina. <br></br>The source code is
+      <a href="http://opensource.org/licenses/mit-license.php"> MIT</a> licensed. The website content
       uses graphic material <a href="http://www.anitamikas.com"> designed by anitam, </a> <a href="http://www.freepik.com"> slidesgo and </a>
       <a href="http://www.freepik.com">rawpixel.com / Freepik</a>
     </p>
